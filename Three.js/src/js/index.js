6d129c0cc52581d9dbc3cd5c2994e7d580b5fdc2
mingddo <godhsb@naver.com>
2023-08-08 17:13:43 +0900

    나무 오브젝트 생성

--- Three.js/src/js/index.js
+++ Three.js/src/js/index.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import WebGL from "../../node_modules/three/examples/jsm/capabilities/WebGL.js";
 
-const $result = document.getElementById('result');
 
 if (WebGL.isWebGLAvailable()) {
 
@@ -16,50 +15,130 @@ if (WebGL.isWebGLAvailable()) {
     // const camera = new THREE.PerspectiveCamera(50, $result.clientWidth / $result.clientHeight, 0.1, 10000);
     const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
 
-    camera.position.set(3,3,3);
-    camera.lookAt(0,0,0,)
+    camera.position.set(10, 10, 10);
+    camera.lookAt(0, 0, 0,)
 
 
     // 3. Renderer: Scene + Camera, 화면을 그려주는 열할
-    const renderer = new THREE.WebGLRenderer({canvas: $result, antialias:true});
-    renderer.setSize($result.clientWidth, $result.clientHeight);
-    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
 
     const light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(2,4,3);
+    light.position.set(2, 4, 3);
     scene.add(light);
 
-    
-    const geometry = new THREE.DodecahedronGeometry(1);
-    const material = new THREE.MeshStandardMaterial({
-        color: 0xffaaaa,
+
+    // =============================
+    // 한라봉
+
+    // const bodyMaterial = new THREE.MeshStandardMaterial({
+    //     color : 0xff7f00
+
+    // })
+
+    // const bottomGeometry = new THREE.DodecahedronGeometry(2,1);
+    // const bottom = new THREE.Mesh(bottomGeometry, bodyMaterial);
+
+    // scene.add(bottom);
+
+
+    // const topGeometry = new THREE.TetrahedronGeometry(0.8,3);
+    // const top = new THREE.Mesh(topGeometry, bodyMaterial);
+    // scene.add(top);
+    // top.position.y = 1.7;
+
+    // const leafMaterial = new THREE.MeshStandardMaterial({
+    //     color: 0x008000,
+    //     side: THREE.DoubleSide,
+    // });
+
+    // const stemGeometry = new THREE.CylinderGeometry(0.2, 0.2, 0.5);
+    // const stem = new THREE.Mesh(stemGeometry, leafMaterial);
+    // scene.add(stem);
+    // stem.position.y = 2.5;
+
+    // const leafGeometry = new THREE.SphereGeometry(0.5, 32, 16, 0, Math.PI /3 );
+    // const leaf = new THREE.Mesh(leafGeometry, leafMaterial);
+    // scene.add(leaf);
+    // leaf.position.set(-0.5, 2.4, -0.1);
+    // leaf.rotation.z = Math.PI / -2;
+
+
+
+    // =============================
+    // 야자수
+
+    const trunkMaterial = new THREE.MeshStandardMaterial({
+        color: 0xa38049
     })
 
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    const trunkGeometry = new THREE.CylinderGeometry(0.8, 1, 1.5);
+    const trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    scene.add(trunk1);
 
 
-    // 1. 위치
-    mesh.position.set(0,2,1);
+    const trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    trunk2.position.set(0.1, 1.3, 0);
+    trunk2.scale.set(0.9, 0.9, 0.9);
+    trunk2.rotation.z = THREE.MathUtils.degToRad(-5);
+    scene.add(trunk2);
 
 
-    // 2. 회전
-    mesh.rotation.y = THREE.MathUtils.degToRad(30);
+    const trunk3 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    trunk3.position.set(0.2, 2.5, 0);
+    trunk3.scale.set(0.8, 0.8, 0.8);
+    trunk3.rotation.z = THREE.MathUtils.degToRad(-5);
+    scene.add(trunk3);
+
+    const trunk4 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    trunk4.position.set(0.3, 3.5, 0);
+    trunk4.scale.set(0.7, 0.7, 0.7);
+    trunk4.rotation.z = THREE.MathUtils.degToRad(-8);
+    scene.add(trunk4);
 
 
+    const leafMaterial = new THREE.MeshStandardMaterial({
+        color: 0x84ad88,
+        side: THREE.DoubleSide
+    })
+
+    const leafGeometry = new THREE.SphereGeometry(2, 32, 16, Math.PI / 3, Math.PI / 3);
+    
+    const leaf1 = new THREE.Mesh(leafGeometry, leafMaterial);
+    leaf1.rotation.x = Math.PI / -2;
+    leaf1.position.set(0, 3.2, 2);
+    scene.add(leaf1);
+
+    const leaf2 = new THREE.Mesh(leafGeometry, leafMaterial);
+    leaf2.rotation.x = Math.PI / -2;
+    leaf2.rotation.z = Math.PI / 2;
+    leaf2.position.set(2, 3.2, 0);
+    scene.add(leaf2);
+
+    const leaf3 = new THREE.Mesh(leafGeometry, leafMaterial);
+    leaf3.rotation.x = Math.PI / -2;
+    leaf3.rotation.z = Math.PI;
+    leaf3.position.set(0, 3.2, -2);
+    scene.add(leaf3);
+
+    const leaf4 = new THREE.Mesh(leafGeometry, leafMaterial);
+    leaf4.rotation.x = Math.PI / -2;
+    leaf4.rotation.z = Math.PI / -2;
+    leaf4.position.set(-2, 3.2, 0);
+    scene.add(leaf4);
 
 
     // axesHelper
-    const axesHelper = new THREE.AxesHelper(10);
-    scene.add(axesHelper);
+    const axes = new THREE.AxesHelper(10);
+    scene.add(axes);
 
-
+    
 
     // OrbitControls
     const controls = new OrbitControls(camera, renderer.domElement);
 
-    
 
     // 조작 설정
     // controls
@@ -68,7 +147,7 @@ if (WebGL.isWebGLAvailable()) {
 
     function animate() {
         // box.rotation.y += 0.01;
-        
+
         requestAnimationFrame(animate);
         controls.update();
         renderer.render(scene, camera);
@@ -78,7 +157,7 @@ if (WebGL.isWebGLAvailable()) {
 
     window.addEventListener("resize", () => {
         // 1. 카메라의 종횡비
-        camera.aspect = window.innerWidth/window.innerHeight
+        camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix(); // 카메라 업데이트
 
         // 2. 렌더러의 크기
